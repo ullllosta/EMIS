2adb1df701616e34ffa7c120f1c6008a0a1430ed
ulllosta <lizayakushova172@gmail.com>
2024-02-07 22:57:44 +0300

    add slider, and more changes

---
+++ scripts/products-slider.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 5,
+    loop: true,
+    grabCursor: true,
+    loopAdditionalSlides: 4,
+    centeredSlides: true,
+    roundLengths: true,
+    slideActiveClass: 'active',
+  });
